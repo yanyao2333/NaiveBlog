e5801f46d2a458c76258aeaa4cb32b9e506daec9
roitium <65794453+yanyao2333@users.noreply.github.com>
2024-09-17 16:06:44 +0800

    [feat]add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,6 +12,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/projects/hfs-next.jpeg',
     href: 'https://github.com/yanyao2333/hfs-next',
   },
+  {
+    title: 'BiliGPTHelper',
+    description: `A Bilibili tool for summarizing videos using ChatGPT.`,
+    imgSrc: '/static/images/projects/bili-gpt-helper.png',
+    href: 'https://github.com/yanyao2333/BiliGPTHelper',
+  },
 ]
 
 export default projectsData
