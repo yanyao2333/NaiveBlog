de4b36df428f59af0ffc2d908595212c5ebfb2c9
roitium <65794453+yanyao2333@users.noreply.github.com>
2024-10-06 21:27:03 +0800

    [fix]add memo components

---
+++ components/homeComponents/RecentlyMemos.tsx
@@ -0,0 +1,56 @@
+'use client'
+import moment from 'moment/min/moment-with-locales'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { Memo, MemoListResponse } from '../../types/memos'
+
+async function fetchMemos() {
+  if (!process.env.NEXT_PUBLIC_MEMOS_ENDPOINT) {
+    return []
+  }
+  const apiEndpoint = process.env.NEXT_PUBLIC_MEMOS_ENDPOINT?.replace(/\/$/, '')
+  // 逆天参数，给我整不会了
+  const filter = `filter=creator=='users/1'`
+  const pageSize = 'pageSize=3'
+  const apiPath = 'api/v1/memos'
+  const response = await fetch(`${apiEndpoint}/${apiPath}?${filter}&${pageSize}`)
+  const jsonResp: MemoListResponse = await response.json()
+  return jsonResp.memos
+}
+
+export default function RecentlyMemos() {
+  const router = useRouter()
+  const [memos, setMemos] = useState<Memo[]>([])
+  useEffect(() => {
+    moment.locale(navigator.language)
+    fetchMemos().then((data) => setMemos(data))
+  }, [])
+
+  return (
+    <Link
+      className="flex cursor-pointer flex-col rounded-lg bg-primary-200 shadow-sm transition-shadow hover:shadow-xl dark:bg-gray-500 sm:col-span-2"
+      href={'/memory'}
+    >
+      <span className="ml-3 mt-2">💡 最近想法</span>
+      <div className="prose m-3 flex min-w-fit flex-col divide-y divide-dashed rounded-md bg-white px-3 shadow-lg prose-p:my-0">
+        {memos
+          ? memos.map((memo) => {
+              console.log(memo)
+              return (
+                <div className="py-3" key={memo.uid}>
+                  {memo.snippet.length > 30 ? memo.snippet.substring(0, 30) + '...' : memo.snippet}
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
+                    {moment(memo.createTime).fromNow()}
+                  </p>
+                </div>
+              )
+            })
+          : null}
+      </div>
+      <button className="mb-2 ml-auto mr-4" onClick={() => router.push('/memory')}>
+        查看更多 &rarr;
+      </button>
+    </Link>
+  )
+}
