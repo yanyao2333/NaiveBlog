ae434a43407a6caba8b359b142a30da765f9a8fa
roitium <65794453+yanyao2333@users.noreply.github.com>
2024-10-27 11:21:47 +0800

    [fix]make prettier happy

--- components/LightGalleryWrapper.tsx
+++ components/LightGalleryWrapper.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 'use client'
-import 'lightgallery/css/lightgallery.css'
-import 'lightgallery/css/lg-zoom.css'
 import 'lightgallery/css/lg-thumbnail.css'
+import 'lightgallery/css/lg-zoom.css'
+import 'lightgallery/css/lightgallery.css'
 import lgThumbnail from 'lightgallery/plugins/thumbnail'
 import lgZoom from 'lightgallery/plugins/zoom'
 import LightGallery from 'lightgallery/react'
