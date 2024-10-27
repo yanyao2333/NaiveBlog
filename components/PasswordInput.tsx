b502d512bcc5b17b08832c1d02b103c8e4a258da
roitium <65794453+yanyao2333@users.noreply.github.com>
2024-10-27 18:33:20 +0800

    [fix]make a11y happy on password component

--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -10,7 +10,7 @@ import toast from 'react-hot-toast'
 export default function PasswordInput() {
   const [password, setPassword] = useState('')
   const path = usePathname()
-  const [verifed, setVerified] = useState(false)
+  const [verified, setVerified] = useState(false)
   const [blog, setBlog] = useState<Blog | undefined>(undefined)
 
   function verifyPassword(password: string, path: string) {
@@ -31,25 +31,32 @@ export default function PasswordInput() {
       })
   }
 
-  if (verifed && blog) {
+  if (verified && blog) {
     return <MDXLayoutRenderer code={blog.body.code} components={components} toc={blog.toc} />
   }
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center w-full">
       本文需要密码才能查看，请输入密码~
-      <div className="mt-3">
+      <div className="mt-3 flex w-full justify-center">
         <input
-          type="text"
-          className="rounded border-0 bg-gray-200 px-4 py-2 text-gray-700 dark:bg-gray-800 dark:text-gray-300"
+          type="password"
+          className="rounded-l max-w-[200px] sm:max-w-[300px] border border-gray-500 border-r-0 bg-gray-200 px-4 py-2 text-gray-700 dark:bg-neutral-700 dark:text-neutral-300"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <input
-          type="submit"
-          className="ml-2 cursor-pointer rounded border-0 bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-          value="提交"
+        <div
+          role="button"
+          tabIndex={0}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              verifyPassword(password, path)
+            }
+          }}
+          className="w-12 text-center border border-l-0 flex-shrink-0 border-gray-500 content-center cursor-pointer rounded-r bg-primary-400 dark:bg-primary-600 dark:hover:bg-primary-700font-bold text-white hover:bg-primary-700"
           onClick={() => verifyPassword(password, path)}
-        />
+        >
+          &rarr;
+        </div>
       </div>
     </div>
   )
