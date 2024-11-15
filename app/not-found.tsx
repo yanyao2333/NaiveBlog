7310597e5745f3ea7c94480b8ff0ca401a4817d0
roitium <65794453+yanyao2333@users.noreply.github.com>
2024-11-15 19:24:31 +0800

    [fix]404 page style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,15 @@
 export default function NotFound() {
   return (
-    <div className="flex min-h-[calc(100dvh-5rem)] flex-col items-start justify-start md:flex-row md:items-center md:justify-center md:space-x-6">
+    <div className="flex min-h-[calc(100dvh-5rem)] flex-col justify-center items-center md:flex-row md:space-x-6">
       <div className="space-x-2 pb-8 pt-6 md:space-y-5">
         <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
           404
         </h1>
       </div>
       <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">你来到了神隐之地</p>
+        <p className="mb-4 text-base text-center font-bold leading-normal md:text-xl">
+          你来到了神隐之地
+        </p>
         {/*<p className="mb-8">别乱跑！否则就回不去了哦~</p>*/}
         {/*<Link*/}
         {/*  href="/"*/}
