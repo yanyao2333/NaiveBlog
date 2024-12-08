0483850bde64bd708eebd81fafedb3ab4807d628
roitium <65794453+yanyao2333@users.noreply.github.com>
2024-12-08 12:39:37 +0800

    [fix](mdx-plugins/toc)use GithubSlugger class instead of slug function

--- src/mdx-plugins/toc.ts
+++ src/mdx-plugins/toc.ts
@@ -1,5 +1,5 @@
 /** From https://github.com/timlrx/pliny */
-import { slug } from 'github-slugger'
+import GithubSlugger from 'github-slugger'
 import { Heading } from 'mdast'
 import { toString } from 'mdast-util-to-string'
 import { remark } from 'remark'
@@ -14,17 +14,17 @@ export type TocItem = {
 }
 
 export type Toc = TocItem[]
+const slugger = new GithubSlugger()
 
 /**
  * Extracts TOC headings from markdown file and adds it to the file's data object.
- * 我修改了一下，去除掉了包装textContent的slug，因为他会在我的链接后面加一个自增的数字，导致链接不准确。我不确定这是不是预期行为，但总之在我这跑不了。
  */
 export function remarkTocHeadings() {
   return (tree: Parent, file: VFile) => {
     const toc: Toc = []
     visit(tree, 'heading', (node: Heading) => {
       const textContent = toString(node)
-      const slugUrl = slug(textContent)
+      const slugUrl = slugger.slug(textContent)
       toc.push({
         value: textContent,
         url: '#' + slugUrl,
