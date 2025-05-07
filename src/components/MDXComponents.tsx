bfcd11f82f12f14fe52e16f5c5cbc71c783c2107
roitium <65794453+yanyao2333@users.noreply.github.com>
2025-05-07 22:12:42 +0800

    feat: mdx components

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -4,6 +4,7 @@ import TOCInline from '@/components/TOC'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
+import GCPlay from '@/components/mdxContentComponents/gc'
 
 export const components: MDXComponents = {
   Image,
@@ -11,4 +12,5 @@ export const components: MDXComponents = {
   a: CustomLink,
   pre: Pre,
   table: TableWrapper,
+  GCPlay: GCPlay
 }
